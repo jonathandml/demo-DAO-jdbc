272f9259620c4ab1feb1dbd87c230eb74901a2a8
Jonathan Lopes <jdlopes.97@outlook.com>
2020-03-15 11:14:26 -0300

    classe seller criada

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department d = new Department(1,"books");
+		
+		Seller s = new Seller(1, "Bob", "Bob@email.com", new Date(), 3000.0, d);
+		System.out.println();
 		System.out.println(d);
+		System.out.println(s);
 
 	}
 
